741d834833a6f388d4944cd287ce1e52685ff67c
Kogersh1n <k09ersh1n@gmail.com>
2026-06-15 21:06:42 +0500

    feat: connect useSongs hook to real API and add youtube import

--- frontend/src/features/songs/useSong.ts
+++ frontend/src/features/songs/useSong.ts
@@ -1,24 +1,63 @@
-import {useState, useEffect} from 'react';
-import {loadSongs} from '../../api/songs';
+import {useState, useEffect, useCallback} from 'react';
+import {loadSongs, importSongFromYoutube, type ApiSong} from '../../api/songs';
 
-import { MOCK_SONGS } from '../../mockData.ts';
+// import { MOCK_SONGS } from '../../mockData.ts';
 
-export interface Song {
-    id: number;
-    title: string;
-    author: string;
-    url: string;
-}
 
 export function useSongs() {
-    const [songs, setSongs] = useState(MOCK_SONGS);
-    
-    // const [isLoading, setIsLoading] = useState(true);
-    
-    // const [error, setError] = useState<string | null>(null);
+    const [songs, setSongs] = useState<ApiSong[]>([]);    
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [error, setError] = useState<string | null>(null);
 
-    const [isLoading] = useState(false);
-    const [error] = useState<string | null>(null);
+    // states for infinity scrolling
+    const [cursor, setCursor] = useState<string | null>(null);
+    const [hasMore, setHasMore] = useState<boolean>(false);
+
+    const fetchSongs = useCallback(async (currentCursor?: string | null) => {
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            const data = await loadSongs(currentCursor)
+
+            setSongs(data.items);
+            setCursor(data.next_cursor);
+            setHasMore(data.has_more);            
+        } catch (err: any) {
+            setError(err.message || 'Can not load songs')
+        } finally {
+            setIsLoading(false);
+        }
+    }, []);
+
+    const importTrack = async (query: string) => {
+        setIsLoading(true);
+        setError(null);
+
+        try {
+            await importSongFromYoutube(query);
+
+            await fetchSongs();
+        } catch (err: any) {
+            setError(err.message || 'Error with import');
+            setIsLoading(false);
+        }
+    };
+
+    useEffect (() => {
+        fetchSongs();
+
+    }, [fetchSongs]);
+
+    return {
+        songs,
+        isLoading,
+        error,
+        importTrack,
+        hasMore,
+        load_more: () => hasMore && fetchSongs(cursor),
+    };
+
 
     // useEffect(() => {
     //     async function fetchSongs() {
@@ -42,5 +81,4 @@ export function useSongs() {
 
     // }, []);
 
-    return  {songs, isLoading, error};
 }
